06be6b2163002fe0c4631e4f2e2354a0394bcc88
Neel Bhatt <vishvamx@gmail.com>
2023-10-03 03:44:22 +0530

    Ltp Changed

--- Logic/AngelOne/GetLtp.py
+++ Logic/AngelOne/GetLtp.py
@@ -1,23 +1,23 @@
 from SmartApi import SmartConnect
 from App.DataHub import get_config_obj
 
-# def getLTP(TOKEN, SYMBOL):
-#     configInfo = get_config_obj()
-
-#     obj=SmartConnect(
-#         api_key=configInfo.AngleOneApiKey, 
-#         access_token=configInfo.AngleOneAccessToken,
-#         refresh_token=configInfo.AngleOneRefreshToken,
-#         feed_token=configInfo.AngleOneFeedToken,
-#         userId=configInfo.AngleOneUserId
-#     )
-
-#     instrument_data = obj.ltpData('NFO', SYMBOL, TOKEN)
-#     ltp = instrument_data['data']['ltp']
-#     return ltp
-
-
 def getLTP(TOKEN, SYMBOL):
-    f = open('/Users/neel/T2/tmpLTP.txt', 'r')
-    ltp = float(f.read())
+    configInfo = get_config_obj()
+
+    obj=SmartConnect(
+        api_key=configInfo.AngleOneApiKey, 
+        access_token=configInfo.AngleOneAccessToken,
+        refresh_token=configInfo.AngleOneRefreshToken,
+        feed_token=configInfo.AngleOneFeedToken,
+        userId=configInfo.AngleOneUserId
+    )
+
+    instrument_data = obj.ltpData('NFO', SYMBOL, TOKEN)
+    ltp = instrument_data['data']['ltp']
     return ltp
+
+
+# def getLTP(TOKEN, SYMBOL):
+#     f = open('/Users/neel/T2/tmpLTP.txt', 'r')
+#     ltp = float(f.read())
+#     return ltp
